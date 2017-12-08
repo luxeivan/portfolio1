d9ae122aa119973a138ef2782884351b85d63990
luxeivan <luxeivan@gmail.com>
2017-12-09 00:56:39 +0300

    run project

---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+
+const del = require('del');
+
+const browserSync = require('browser-sync').create();
+
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+
+const paths = {
+    root: './build',
+    templates: {
+        pages: 'src/templates/pages/*.pug',
+        src: 'src/templates/**/*.pug'
+    },
+    styles: {
+        src: 'src/styles/**/*.scss',
+        dest: 'build/assets/styles/'
+    },    
+    images: {
+        src: 'src/images/**/*.*',
+        dest: 'build/assets/images/'
+    },
+    fonts: {
+        src: 'src/fonts/**/*.*',
+        dest: 'build/assets/fonts/'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        dest: 'build/assets/scripts/'
+    }
+}
+
+// pug
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest(paths.root));
+}
+
+// scss
+function styles() {
+    return gulp.src('./src/styles/app.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+// очистка
+function clean() {
+    return del(paths.root);
+}
+
+// webpack
+function scripts() {
+    return gulp.src('src/scripts/app.js')
+        .pipe(plumber())
+        .pipe(gulpWebpack(webpackConfig, webpack)) 
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// галповский вотчер
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.fonts.src, fonts);
+    gulp.watch(paths.scripts.src, scripts);
+}
+
+// локальный сервер + livereload (встроенный)
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
+// просто переносим картинки
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(gulp.dest(paths.images.dest));
+}
+function fonts() {
+    return gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest));
+}
+
+exports.templates = templates;
+exports.styles = styles;
+exports.clean = clean;
+exports.images = images;
+exports.fonts = fonts;
+
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel(styles, templates, images, fonts, scripts),
+    gulp.parallel(server, watch)
+));
